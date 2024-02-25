bdc8b6162a13f523fc439bf808130a144ffe8a2d
jrAzael <160097480+jrAzael@users.noreply.github.com>
2024-02-24 20:13:43 -0600

    Add files via upload

---
+++ operacionesAritmeticas.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main (){
+    // primera suma 
+    printf("la suma de %d + %d es = %d\n", 5,8,5+8);
+    // segunda suma 
+    printf("la suma de %d + %d es = %d\n", 78787,3259,78787+3259);
+
+    return 0;
+}
