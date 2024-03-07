87ebff4320bed4e5c05c242ec528ef09101b09a1
jrAzael <160097480+jrAzael@users.noreply.github.com>
2024-03-06 20:41:12 -0600

    Add files via upload

---
+++ ejerc1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main (){
+    int i=0;
+    do
+    {
+        /* code */
+    printf("valor de i= %d\n",i);
+    i++;
+     } while (i<3);
+
+     printf("fin");
+     return 0;
+    
+}
+/*tirar la pagina del tesji mecaflow
+ataques de 2*/
---
+++ ejerc2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main (){
+
+    int vector[10];
+    int contador=0;
+   while (contador <10){
+    vector[contador]=10;
+        printf("%d\n",vector[contador]);
+        contador++;
+    }
+     return 0;
+    
+}
